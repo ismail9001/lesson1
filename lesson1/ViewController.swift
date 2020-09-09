090c6847dd56012b1fc0fbe136d2c55288dc66d6
Iskander Nizamov <nizamov-iskander@yandex.ru>
2020-09-09 18:01:51 +0300

    homework 1

--- lesson1/ViewController.swift
+++ lesson1/ViewController.swift
@@ -12,7 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //1 квадратное уравнение x2 − 8x + 12 = 0;
+        
+        let koef1 = 1.0;
+        let koef2 = -8.0;
+        let koef3 = 12.0;
+        let d = koef2 * koef2 - 4 * koef1 * koef3;
+        print ("discriminant = " + String(d));
+        
+        // дискриминант > 0, следовательно, имеем два корня
+        let x1 = koef2 * -1 + sqrt(d) / 2 * koef1;
+        let x2 = koef2 * -1 - sqrt(d) / 2 * koef1;
+        
+        print ("root 1 = " + String(x1));
+        print ("root 2 = " + String(x2));
+        
+        //2 найти параметры треугольника
+        
+        let a = 3.0;
+        let b = 4.0;
+        
+        //гипотенуза
+        let c = sqrt(a * a + b * b);
+        print("hypotenuse = " + String(c));
+        
+        //периметр
+        let perimeter = a + b + c;
+        print("perimeter = " + String(perimeter));
+        
+        // площадь по формуле Герона
+        let half = perimeter / 2;
+        let square = sqrt(half * (half - a) * (half - b) * (half - c));
+        print("square = " + String(square));
+        
+        //3 найти сумму вклада через 5 лет
+        
+        var depo = 1000.0;
+        let percent = 10.0;
+        for _ in 1...5 {
+            depo = depo + depo / 100 * percent;
+        }
+        print ("depo after 5 years = " + String(format: "%.2f", depo));
     }
 
 
